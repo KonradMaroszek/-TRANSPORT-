a8c6fa67feec3418f1ddf365339e018b85f441f5
KonradMaroszek <konradmaroszek@interia.eu>
2017-01-06 05:14:36 +0100

    Dodanie strzałek, imitacji logowania, trasy

--- AplikacjaTransport/src/Commons/TabGuardian.java
+++ AplikacjaTransport/src/Commons/TabGuardian.java
@@ -2,22 +2,34 @@ package Commons;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class TabGuardian {	
 	
 	HashMap<String,JPanel> tabPanels;
-	
+	HashMap<String,ImageIcon> icons;
+
 	public TabGuardian(){
 		tabPanels = new HashMap<String,JPanel>();
+		icons = new HashMap<String,ImageIcon>();
 	}
 	
 	public void addPanelToMap (String name, JPanel panel){
 		tabPanels.put(name, panel);
 	}
 	
+	public void addIconToMap (String name, ImageIcon panel){
+		icons.put(name, panel);
+	}
+	
 	public HashMap<String,JPanel> getPanelMap(){
 		return tabPanels;
+	}
+	
+	public HashMap<String, ImageIcon> getIconsMap() {
+		return icons;
 	}
 	
 	public HashMap<String,JPanel> getPanelsByNames (ArrayList<String> panelsNames){
@@ -29,5 +41,16 @@ public class TabGuardian {
 		}
 		
 		return tabPanelsMap;
+	}
+	
+	public HashMap<String,ImageIcon> getIconsByNames (ArrayList<String> iconsNames){
+		
+		HashMap<String,ImageIcon> tabIconsMap  = new HashMap<String,ImageIcon>();
+		
+		for (String iconName : iconsNames){
+			tabIconsMap.put(iconName, icons.get(iconName));
+		}
+		
+		return tabIconsMap;
 	}
 }
--- AplikacjaTransport/src/GUI/MainWindow.java
+++ AplikacjaTransport/src/GUI/MainWindow.java
@@ -21,6 +21,7 @@ import java.awt.Dimension;
 import javax.swing.SwingConstants;
 
 import Commons.TabGuardian;
+import Commons.UserLogged;
 import Database.DatabaseConnection;
 import Database.Execute_command;
 import Database.FetchQuery;
@@ -57,6 +58,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
 
 public class MainWindow {
 
@@ -144,6 +146,36 @@ public class MainWindow {
 	TabGuardian tabGuardian;
 	ArrayList<String> panelsNames;
 	JTabbedPane tabPanel;
+	private JTextField textField;
+	private JPasswordField passwordField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+	private JTextField textField_9;
+	private JTextField textField_10;
+	private JTextField textField_11;
+	private JTextField textField_12;
+	private JTextField textField_13;
+	private JTextField textField_14;
+	private JTextField textField_15;
+	private JTextField textField_16;
+	private JTextField textField_17;
+	private JTextField textField_18;
+	private JTextField textField_19;
+	private JTextField textField_20;
+	private JTextField textField_21;
+	private JTextField textField_22;
+	private JTextField textField_23;
+	private JTextField textField_24;
+	private JTextField textField_25;
+	private JTextField textField_26;
+	private JTextField textField_27;
+	private JTextField textField_28;
 
 	/**
 	 * Launch the application.
@@ -182,19 +214,86 @@ public class MainWindow {
 		frmAplikacjaTransport.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmAplikacjaTransport.getContentPane().setLayout(new BoxLayout(frmAplikacjaTransport.getContentPane(), BoxLayout.X_AXIS));
 		
-		JTabbedPane TabPanel = tabPanel = new JTabbedPane(JTabbedPane.TOP);
+		JTabbedPane TabPanel = new JTabbedPane(JTabbedPane.TOP);
+		tabPanel = TabPanel;
 		TabPanel.setBackground(Color.LIGHT_GRAY);
 		frmAplikacjaTransport.getContentPane().add(TabPanel);
 		
 		JPanel MainMenuPanle = new JPanel();
-		tabGuardian.addPanelToMap("MainMenu", MainMenuPanle);
-		panelsNames.add("MainMenu");
+		
+		tabGuardian.addPanelToMap("Menu", MainMenuPanle);
+		tabGuardian.addIconToMap("Menu", new ImageIcon(MainWindow.class.getResource("/resources/menu.png")));
+		panelsNames.add("Menu");
+		
 		TabPanel.addTab("Menu g\u0142\u00F3wne", new ImageIcon(MainWindow.class.getResource("/resources/menu.png")), MainMenuPanle, null);
-		MainMenuPanle.setLayout(null);
+		MainMenuPanle.setLayout(new MigLayout("", "[][grow]", "[][][]"));
+		
+		JLabel lblLogin = new JLabel("Login");
+		MainMenuPanle.add(lblLogin, "cell 0 0,alignx trailing");
+		
+		textField = new JTextField();
+		MainMenuPanle.add(textField, "cell 1 0,growx");
+		textField.setColumns(10);
+		
+		JLabel lblHaso = new JLabel("Has\u0142o");
+		MainMenuPanle.add(lblHaso, "cell 0 1,alignx trailing");
+		
+		passwordField = new JPasswordField();
+		MainMenuPanle.add(passwordField, "cell 1 1,growx");
+		
+		JButton btnNewButton_1 = new JButton("Zaloguj");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// TUTAJ SPRAWDZASZ KTO SIÊ LOGUJE!  
+				
+				UserLogged.setUserType(UserLogged.UserType.ADMIN); // przypisujesz enuma tutaj
+				
+				HashMap<String, JPanel> panels = tabGuardian.getPanelMap();
+				HashMap<String, ImageIcon> icons = tabGuardian.getIconsMap();
+				
+				if (UserLogged.getUserType() == UserLogged.UserType.ADMIN){
+					panels.remove("Menu");
+					icons.remove("Menu");
+					
+					for ( String name : panels.keySet()){
+						tabPanel.addTab(name, icons.get(name), panels.get(name), null);
+					}
+					
+				}else if (UserLogged.getUserType() == UserLogged.UserType.EMPLOYEE){
+					panels.remove("Menu");
+					icons.remove("Menu");
+					
+					panels.remove("Kontrahenci");
+					icons.remove("Kontrahenci");
+					
+					for ( String name : panels.keySet()){
+						tabPanel.addTab(name, icons.get(name), panels.get(name), null);
+					}
+					
+				}else if (UserLogged.getUserType() == UserLogged.UserType.FORWARDER){
+					panels.remove("Menu");
+					icons.remove("Menu");
+					
+					panels.remove("Kontrahenci");
+					icons.remove("Kontrahenci");
+					
+					panels.remove("Pracownicy");
+					icons.remove("Pracownicy");
+					
+					for ( String name : panels.keySet()){
+						tabPanel.addTab(name, icons.get(name), panels.get(name), null);
+					}
+				}
+			}
+		});
+		btnNewButton_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		MainMenuPanle.add(btnNewButton_1, "cell 1 2");
 		
 		JPanel EmployeesPanel = new JPanel();
-		tabGuardian.addPanelToMap("Employees", EmployeesPanel);
-		panelsNames.add("Employees");
+		tabGuardian.addPanelToMap("Pracownicy", EmployeesPanel);
+		tabGuardian.addIconToMap("Pracownicy", new ImageIcon(MainWindow.class.getResource("/resources/pracownicy.png")));
+		panelsNames.add("Pracownicy");
+		
 		TabPanel.addTab("Pracownicy", new ImageIcon(MainWindow.class.getResource("/resources/pracownicy.png")), EmployeesPanel, null);
 		
 		JLabel EmployeesPhotoLabel = new JLabel("");
@@ -660,9 +759,20 @@ public class MainWindow {
 		EmployessMainRadioButtonEditMode.setBounds(347, 38, 200, 23);
 		EmployeesPanel.add(EmployessMainRadioButtonEditMode);
 		
+		JButton button_4 = new JButton("");
+		button_4.setIcon(new ImageIcon(MainWindow.class.getResource("/images32x32/leftarrow32.png")));
+		button_4.setBounds(773, 11, 144, 35);
+		EmployeesPanel.add(button_4);
+		
+		JButton button_5 = new JButton("");
+		button_5.setIcon(new ImageIcon(MainWindow.class.getResource("/images32x32/rightarrow32.png")));
+		button_5.setBounds(915, 11, 144, 35);
+		EmployeesPanel.add(button_5);
+		
 		JPanel VehiclesPanel = new JPanel();
-		tabGuardian.addPanelToMap("Vehicles", VehiclesPanel);
-		panelsNames.add("Vehicles");
+		tabGuardian.addPanelToMap("Pojazdy", VehiclesPanel);
+		tabGuardian.addIconToMap("Pojazdy", new ImageIcon(MainWindow.class.getResource("/resources/truck.png")));
+		panelsNames.add("Pojazdy");
 		TabPanel.addTab("Pojazdy", new ImageIcon(MainWindow.class.getResource("/resources/truck.png")), VehiclesPanel, null);
 		VehiclesPanel.setLayout(null);
 		
@@ -1102,9 +1212,21 @@ public class MainWindow {
 		VehiclesMainTextCapacity.setBounds(348, 190, 200, 20);
 		VehiclesPanel.add(VehiclesMainTextCapacity);
 		
+		JButton btnNewButton_2 = new JButton("");
+		btnNewButton_2.setIcon(new ImageIcon(MainWindow.class.getResource("/images32x32/leftarrow32.png")));
+		btnNewButton_2.setSelectedIcon(new ImageIcon(MainWindow.class.getResource("/images32x32/Mechanikas.png")));
+		btnNewButton_2.setBounds(772, 5, 144, 35);
+		VehiclesPanel.add(btnNewButton_2);
+		
+		JButton btnNastpny = new JButton("");
+		btnNastpny.setIcon(new ImageIcon(MainWindow.class.getResource("/images32x32/rightarrow32.png")));
+		btnNastpny.setBounds(914, 5, 144, 35);
+		VehiclesPanel.add(btnNastpny);
+		
 		JPanel InstitutionPanel = new JPanel();
-		tabGuardian.addPanelToMap("Institution", InstitutionPanel);
-		panelsNames.add("Institution");
+		tabGuardian.addPanelToMap("Plac\u00F3wki", InstitutionPanel);
+		tabGuardian.addIconToMap("Plac\u00F3wki", new ImageIcon(MainWindow.class.getResource("/resources/fabryka.png")));
+		panelsNames.add("Plac\u00F3wki");
 		TabPanel.addTab("Plac\u00F3wki", new ImageIcon(MainWindow.class.getResource("/resources/fabryka.png")), InstitutionPanel, null);
 		InstitutionPanel.setLayout(null);
 		
@@ -1547,14 +1669,321 @@ public class MainWindow {
 		InstytutionMainTextCountry.setBounds(679, 205, 200, 20);
 		InstitutionPanel.add(InstytutionMainTextCountry);
 		
+		JButton button_2 = new JButton("");
+		button_2.setIcon(new ImageIcon(MainWindow.class.getResource("/images32x32/leftarrow32.png")));
+		button_2.setBounds(773, 6, 144, 35);
+		InstitutionPanel.add(button_2);
+		
+		JButton button_3 = new JButton("");
+		button_3.setIcon(new ImageIcon(MainWindow.class.getResource("/images32x32/rightarrow32.png")));
+		button_3.setBounds(915, 6, 144, 35);
+		InstitutionPanel.add(button_3);
+		
 		JPanel RoutePanel = new JPanel();
-		tabGuardian.addPanelToMap("Route", RoutePanel);
-		panelsNames.add("Route");
+		tabGuardian.addPanelToMap("Trasy", RoutePanel);
+		tabGuardian.addIconToMap("Trasy", new ImageIcon(MainWindow.class.getResource("/resources/mapa.png")));
+		panelsNames.add("Trasy");
 		TabPanel.addTab("Trasy", new ImageIcon(MainWindow.class.getResource("/resources/mapa.png")), RoutePanel, null);
+		RoutePanel.setLayout(null);
+		
+		JButton btnWyszukajTrase = new JButton("wyszukaj trase");
+		btnWyszukajTrase.setBounds(888, 94, 171, 23);
+		RoutePanel.add(btnWyszukajTrase);
+		
+		JButton btnDodajTrase = new JButton("dodaj trase");
+		btnDodajTrase.setBounds(888, 131, 171, 23);
+		RoutePanel.add(btnDodajTrase);
+		
+		JButton btnUsuTrase = new JButton("usu\u0144 trase");
+		btnUsuTrase.setBounds(888, 165, 171, 23);
+		RoutePanel.add(btnUsuTrase);
+		
+		JRadioButton radioButton = new JRadioButton("tryb edycji");
+		radioButton.setBounds(888, 7, 171, 23);
+		RoutePanel.add(radioButton);
+		
+		JLabel lblMiejsceDocelowe = new JLabel("Miejsce docelowe:");
+		lblMiejsceDocelowe.setBounds(10, 58, 148, 20);
+		RoutePanel.add(lblMiejsceDocelowe);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(610, 94, 190, 20);
+		RoutePanel.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(610, 130, 190, 20);
+		RoutePanel.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(610, 161, 190, 20);
+		RoutePanel.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(610, 192, 190, 20);
+		RoutePanel.add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(610, 223, 190, 20);
+		RoutePanel.add(textField_5);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(610, 257, 190, 20);
+		RoutePanel.add(textField_6);
+		
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		textField_7.setBounds(610, 288, 190, 20);
+		RoutePanel.add(textField_7);
+		
+		JLabel label_1 = new JLabel("Kod pocztowy:");
+		label_1.setBounds(494, 291, 110, 20);
+		RoutePanel.add(label_1);
+		
+		JLabel label_2 = new JLabel("Kraj:");
+		label_2.setBounds(494, 257, 110, 20);
+		RoutePanel.add(label_2);
+		
+		JLabel label_3 = new JLabel("Wojew\u00F3dztwo:");
+		label_3.setBounds(494, 223, 110, 20);
+		RoutePanel.add(label_3);
+		
+		JLabel label_4 = new JLabel("Miasto:");
+		label_4.setBounds(494, 192, 110, 20);
+		RoutePanel.add(label_4);
+		
+		JLabel label_5 = new JLabel("Numer Lokalu:");
+		label_5.setBounds(494, 161, 110, 20);
+		RoutePanel.add(label_5);
+		
+		JLabel label_6 = new JLabel("Ulica:");
+		label_6.setBounds(494, 130, 110, 20);
+		RoutePanel.add(label_6);
+		
+		JLabel label_7 = new JLabel("Nazwa:");
+		label_7.setBounds(494, 94, 100, 20);
+		RoutePanel.add(label_7);
+		
+		JLabel label_8 = new JLabel("Nazwa:");
+		label_8.setBounds(10, 94, 100, 20);
+		RoutePanel.add(label_8);
+		
+		JLabel label_9 = new JLabel("Kod pocztowy:");
+		label_9.setBounds(10, 291, 110, 20);
+		RoutePanel.add(label_9);
+		
+		JLabel label_10 = new JLabel("Kraj:");
+		label_10.setBounds(10, 257, 110, 20);
+		RoutePanel.add(label_10);
+		
+		JLabel label_11 = new JLabel("Wojew\u00F3dztwo:");
+		label_11.setBounds(10, 223, 110, 20);
+		RoutePanel.add(label_11);
+		
+		JLabel label_12 = new JLabel("Miasto:");
+		label_12.setBounds(10, 192, 110, 20);
+		RoutePanel.add(label_12);
+		
+		JLabel label_13 = new JLabel("Numer Lokalu:");
+		label_13.setBounds(10, 161, 110, 20);
+		RoutePanel.add(label_13);
+		
+		JLabel label_14 = new JLabel("Ulica:");
+		label_14.setBounds(10, 130, 110, 20);
+		RoutePanel.add(label_14);
+		
+		textField_8 = new JTextField();
+		textField_8.setColumns(10);
+		textField_8.setBounds(126, 94, 190, 20);
+		RoutePanel.add(textField_8);
+		
+		textField_9 = new JTextField();
+		textField_9.setColumns(10);
+		textField_9.setBounds(126, 130, 190, 20);
+		RoutePanel.add(textField_9);
+		
+		textField_10 = new JTextField();
+		textField_10.setColumns(10);
+		textField_10.setBounds(126, 161, 190, 20);
+		RoutePanel.add(textField_10);
+		
+		textField_11 = new JTextField();
+		textField_11.setColumns(10);
+		textField_11.setBounds(126, 192, 190, 20);
+		RoutePanel.add(textField_11);
+		
+		textField_12 = new JTextField();
+		textField_12.setColumns(10);
+		textField_12.setBounds(126, 223, 190, 20);
+		RoutePanel.add(textField_12);
+		
+		textField_13 = new JTextField();
+		textField_13.setColumns(10);
+		textField_13.setBounds(126, 257, 190, 20);
+		RoutePanel.add(textField_13);
+		
+		textField_14 = new JTextField();
+		textField_14.setColumns(10);
+		textField_14.setBounds(126, 288, 190, 20);
+		RoutePanel.add(textField_14);
+		
+		JLabel lblMiejsceRozpoczciaTrasy = new JLabel("Miejsce rozpocz\u0119cia trasy:");
+		lblMiejsceRozpoczciaTrasy.setBounds(494, 61, 226, 20);
+		RoutePanel.add(lblMiejsceRozpoczciaTrasy);
+		
+		textField_15 = new JTextField();
+		textField_15.setColumns(10);
+		textField_15.setBounds(126, 576, 190, 20);
+		RoutePanel.add(textField_15);
+		
+		JLabel label_15 = new JLabel("Kod pocztowy:");
+		label_15.setBounds(10, 579, 110, 20);
+		RoutePanel.add(label_15);
+		
+		JLabel label_17 = new JLabel("Kraj:");
+		label_17.setBounds(10, 545, 110, 20);
+		RoutePanel.add(label_17);
+		
+		textField_16 = new JTextField();
+		textField_16.setColumns(10);
+		textField_16.setBounds(126, 545, 190, 20);
+		RoutePanel.add(textField_16);
+		
+		textField_17 = new JTextField();
+		textField_17.setColumns(10);
+		textField_17.setBounds(126, 511, 190, 20);
+		RoutePanel.add(textField_17);
+		
+		JLabel label_26 = new JLabel("Wojew\u00F3dztwo:");
+		label_26.setBounds(10, 511, 110, 20);
+		RoutePanel.add(label_26);
+		
+		JLabel label_27 = new JLabel("Miasto:");
+		label_27.setBounds(10, 480, 110, 20);
+		RoutePanel.add(label_27);
+		
+		textField_18 = new JTextField();
+		textField_18.setColumns(10);
+		textField_18.setBounds(126, 480, 190, 20);
+		RoutePanel.add(textField_18);
+		
+		textField_19 = new JTextField();
+		textField_19.setColumns(10);
+		textField_19.setBounds(126, 449, 190, 20);
+		RoutePanel.add(textField_19);
+		
+		JLabel label_28 = new JLabel("Numer Lokalu:");
+		label_28.setBounds(10, 449, 110, 20);
+		RoutePanel.add(label_28);
+		
+		JLabel label_29 = new JLabel("Ulica:");
+		label_29.setBounds(10, 418, 110, 20);
+		RoutePanel.add(label_29);
+		
+		JLabel label_30 = new JLabel("Nazwa:");
+		label_30.setBounds(10, 382, 100, 20);
+		RoutePanel.add(label_30);
+		
+		textField_20 = new JTextField();
+		textField_20.setColumns(10);
+		textField_20.setBounds(126, 382, 190, 20);
+		RoutePanel.add(textField_20);
+		
+		JLabel lblMiejsceZaadunku = new JLabel("Miejsce za\u0142adunku:");
+		lblMiejsceZaadunku.setBounds(10, 349, 148, 20);
+		RoutePanel.add(lblMiejsceZaadunku);
+		
+		textField_21 = new JTextField();
+		textField_21.setColumns(10);
+		textField_21.setBounds(610, 576, 190, 20);
+		RoutePanel.add(textField_21);
+		
+		JLabel label_32 = new JLabel("Kod pocztowy:");
+		label_32.setBounds(494, 579, 110, 20);
+		RoutePanel.add(label_32);
+		
+		JLabel label_33 = new JLabel("Kraj:");
+		label_33.setBounds(494, 545, 110, 20);
+		RoutePanel.add(label_33);
+		
+		textField_22 = new JTextField();
+		textField_22.setColumns(10);
+		textField_22.setBounds(610, 545, 190, 20);
+		RoutePanel.add(textField_22);
+		
+		textField_23 = new JTextField();
+		textField_23.setColumns(10);
+		textField_23.setBounds(610, 511, 190, 20);
+		RoutePanel.add(textField_23);
+		
+		JLabel label_34 = new JLabel("Wojew\u00F3dztwo:");
+		label_34.setBounds(494, 511, 110, 20);
+		RoutePanel.add(label_34);
+		
+		JLabel label_35 = new JLabel("Miasto:");
+		label_35.setBounds(494, 480, 110, 20);
+		RoutePanel.add(label_35);
+		
+		textField_24 = new JTextField();
+		textField_24.setColumns(10);
+		textField_24.setBounds(610, 480, 190, 20);
+		RoutePanel.add(textField_24);
+		
+		textField_25 = new JTextField();
+		textField_25.setColumns(10);
+		textField_25.setBounds(610, 449, 190, 20);
+		RoutePanel.add(textField_25);
+		
+		JLabel label_37 = new JLabel("Numer Lokalu:");
+		label_37.setBounds(494, 449, 110, 20);
+		RoutePanel.add(label_37);
+		
+		JLabel label_38 = new JLabel("Ulica:");
+		label_38.setBounds(494, 418, 110, 20);
+		RoutePanel.add(label_38);
+		
+		JLabel label_39 = new JLabel("Nazwa:");
+		label_39.setBounds(494, 382, 100, 20);
+		RoutePanel.add(label_39);
+		
+		textField_26 = new JTextField();
+		textField_26.setColumns(10);
+		textField_26.setBounds(610, 382, 190, 20);
+		RoutePanel.add(textField_26);
+		
+		JLabel lblMiejsceRozadunku = new JLabel("Miejsce roz\u0142adunku:");
+		lblMiejsceRozadunku.setBounds(494, 349, 148, 20);
+		RoutePanel.add(lblMiejsceRozadunku);
+		
+		textField_27 = new JTextField();
+		textField_27.setColumns(10);
+		textField_27.setBounds(126, 418, 190, 20);
+		RoutePanel.add(textField_27);
+		
+		textField_28 = new JTextField();
+		textField_28.setColumns(10);
+		textField_28.setBounds(610, 413, 190, 20);
+		RoutePanel.add(textField_28);
+		
+		JButton button_6 = new JButton("");
+		button_6.setIcon(new ImageIcon(MainWindow.class.getResource("/images32x32/leftarrow32.png")));
+		button_6.setBounds(10, 7, 144, 35);
+		RoutePanel.add(button_6);
+		
+		JButton button_7 = new JButton("");
+		button_7.setIcon(new ImageIcon(MainWindow.class.getResource("/images32x32/rightarrow32.png")));
+		button_7.setBounds(152, 7, 144, 35);
+		RoutePanel.add(button_7);
 		
 		JPanel ContractorsPanel = new JPanel();
-		tabGuardian.addPanelToMap("Contractors", ContractorsPanel);
-		panelsNames.add("Contractors");
+		tabGuardian.addPanelToMap("Kontrahenci", ContractorsPanel);
+		tabGuardian.addIconToMap("Kontrahenci", new ImageIcon(MainWindow.class.getResource("/resources/uslugi_pracownicy.png")));
+		panelsNames.add("Kontrahenci");
 		TabPanel.addTab("Kontrahenci", new ImageIcon(MainWindow.class.getResource("/resources/uslugi_pracownicy.png")), ContractorsPanel, null);
 		ContractorsPanel.setLayout(null);
 		
@@ -1660,13 +2089,24 @@ public class MainWindow {
 		JPanel panel_12 = new JPanel();
 		tabbedPane_3.addTab("Faktury", new ImageIcon(MainWindow.class.getResource("/images32x32/faktura.png")), panel_12, null);
 		
+		JButton button_12 = new JButton("");
+		button_12.setIcon(new ImageIcon(MainWindow.class.getResource("/images32x32/leftarrow32.png")));
+		button_12.setBounds(740, 11, 144, 35);
+		ContractorsPanel.add(button_12);
+		
+		JButton button_13 = new JButton("");
+		button_13.setIcon(new ImageIcon(MainWindow.class.getResource("/images32x32/rightarrow32.png")));
+		button_13.setBounds(882, 11, 144, 35);
+		ContractorsPanel.add(button_13);
+		
 		JPanel SettingsPanel = new JPanel();
-		tabGuardian.addPanelToMap("Settings", SettingsPanel);
-		panelsNames.add("Settings");
+		tabGuardian.addPanelToMap("Ustawienia", SettingsPanel);
+		tabGuardian.addIconToMap("Ustawienia", new ImageIcon(MainWindow.class.getResource("/resources/ustawienia.png")));
+		panelsNames.add("Ustawienia");
 		TabPanel.addTab("Ustawienia", new ImageIcon(MainWindow.class.getResource("/resources/ustawienia.png")), SettingsPanel, null);	
 
 		HashMap<String, JPanel> panels = tabGuardian.getPanelMap();
-		panels.remove("MainMenu");
+		panels.remove("Menu");
 		
 		for (JPanel panel : panels.values()){
 			tabPanel.remove(panel);
@@ -1849,5 +2289,4 @@ public class MainWindow {
 		
 		
 	}
-	
 }
